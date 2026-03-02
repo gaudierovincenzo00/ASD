3784186cb368ed08bc8143eb8aa6eb4e49105de9
gaudierovincenzo00 <gaudierovincenzo00@gmail.com>
2026-03-02 13:22:39 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ st.cache_resource.clear() # per cache_resource
 
 def app_run():
     add_anagrafica = st.Page("add_anagrafica.py", title="Aggiugni Anagrafica", icon=":material/person_add:")
+    read_anagrafica = st.Page("read_anagrafica.py", title="Leggi Anagrafica", icon=":material/person_add:")
     page = st.navigation(
         {
             "Anagrafica": [add_anagrafica],
