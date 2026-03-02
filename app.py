291c6e7a457fcf41f5f4451b6ca3bc635a501d24
gaudierovincenzo00 <gaudierovincenzo00@gmail.com>
2026-03-02 13:13:13 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import time
 st.cache_data.clear()     # per cache_data
 st.cache_resource.clear() # per cache_resource
 
-st.balloons()
-st.title("hello, welcome!")
-page.run()
+def app_run()
+    add_anagrafica = st.Page("/add_anagrafica.py", title="Aggiugni Anagrafica", icon=":material/person_add:")
+    st.balloons()
+    st.title("hello, welcome!")
 
+app_run()
