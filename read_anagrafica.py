7d94049fc87b974eab4fae7721557fdf10cd9c1e
gaudierovincenzo00 <gaudierovincenzo00@gmail.com>
2026-03-02 13:37:48 +0100

    Add files via upload

--- read_anagrafica.py
+++ read_anagrafica.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
 st.title("read anagrafica")
+
+
+from supabase import create_client, Client
+
+# Recupero delle credenziali dai secrets
+url: str = st.secrets["supabase"]["url"]
+key: str = st.secrets["supabase"]["key"]
+
+# Inizializzazione del client
+supabase: Client = create_client(url, key)
+
+# Esempio di query
+st.title("Dati da Supabase")
+response = supabase.table("nome_tabella").select("*").execute()
+
+st.write(response.data)
